0f2ad12cb23803b92e183fbec191c432e1ba7a4f
the-vuphamlethanh <the.vuphamlethanh@gmail.com>
2022-03-03 23:25:20 -0600

    wtf

--- client/page/productDetail.php
+++ client/page/productDetail.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../assets/style/app.css">
+    <link rel="stylesheet" href="../assets/style/app.mini.css">
     <title>Album Detail</title>
 </head>
 
